b8ed6f90a2f16d816cca0c7a6de0a1a36e9265da
Jesse Freitas <jefre@microsoft.com>
2020-11-24 10:45:46 -0800

    experiment using tf ratios, tfidf WIP

--- code/tfidf_processing.py
+++ code/tfidf_processing.py
@@ -9,23 +9,22 @@ fem_terms = pd.read_csv('./terms/terms_f.csv', header=None)
 # load postings
 data = pd.read_csv('./data/data.csv')
 j = len(data['job_description'])
-out = open('./data/calculated_terms.csv', mode='w', encoding="utf8")
+out = open('./data/calculated_terms_tfidf.csv', mode='w', encoding="utf8")
 
 print('job_title,masc,fem', file=out)
 # for each posting, compute the number of terms that are used from the term lists
+corpus = []
 for i in range(j):
     jd = data['job_description'][i]
     jd_cleaned = bs.BeautifulSoup(jd, features="html.parser")
-    jd_final = jd_cleaned.get_text().lower().split()
-    fem = 0
-    masc = 0
-    for term in jd_final:
-        for m in masc_terms[0]:
-            if term.startswith(m):
-                masc += 1
+    jd_final = jd_cleaned.get_text().lower()
+    corpus.append(jd_final)
 
-        for f in fem_terms[0]:
-            if term.startswith(f):
-                fem += 1
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-    print(str('"' + data['job_title'][i] + '"'),str(masc * 1.0 / len(jd_final)),str(fem * 1.0 / len(jd_final)), sep=',', file=out)
+vectorizer = TfidfVectorizer(min_df=0.0005, max_df=0.9995)
+X = vectorizer.fit_transform(corpus)
+print(len(vectorizer.stop_words_))
+print(len(vectorizer.get_feature_names()))
+print(vectorizer.stop_words_)
+
